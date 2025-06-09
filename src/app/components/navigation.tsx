5976222770c2b90d0e5f20cfce062934804989be
Bilguuntuguldur <bilguuntuguldur@chimege.com>
2025-06-10 00:34:58 +0800

    first commit

---
+++ src/app/components/navigation.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export const Navigation = () => {
+  return (
+    <nav className="flex items-center justify-between p-4 bg-gray-800 text-white">
+      <div className="w-[30px] h-[30px]">
+        <img src={"/logo2.jpg"} />
+      </div>
+    </nav>
+  );
+};
